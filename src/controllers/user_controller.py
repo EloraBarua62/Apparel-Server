dee81ee81432fea9860ef6b67b3d5488f6e4b18b
EloraBarua <elorabarua62@gmail.com>
2025-05-05 23:50:00 +0300

    AP-1 feat: Create login API for authentication

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -6,6 +6,12 @@ from src.services import user_service
 
 router = APIRouter()
 
+# Create a new user
 @router.post("/create/")
 def create_user(user: UserSchema, db: Session = Depends(get_db)):
     return user_service.create_user(db, user)
+
+# 
+@router.post("/login/")
+def get_user(user: UserSchema, db: Session = Depends(get_db)):
+    return user_service.get_user(db, user)
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -4,6 +4,7 @@ from src.models.user_model import User
 from src.schemas.user_schema import UserSchema
 from src.utils.response import api_response
 
+# Function to create a new user
 def create_user(db: Session, user: UserSchema):
     try:
         existed_user = db.query(User).filter(User.email == user.email).first()
@@ -22,7 +23,21 @@ def create_user(db: Session, user: UserSchema):
         return api_response(500, "Failed to add user", str(e))
     
 
-
+# Function to get a user by email
+def get_user(db: Session, user: UserSchema):
+    try:
+        existed_user = db.query(User).filter(User.email == user.email).first()
+        if not existed_user:
+            return api_response(400, "Email not found")
+        else:
+             # Hash the password
+            if bcrypt.checkpw(user.password.encode('utf-8'), existed_user.password.encode('utf-8')):
+                return api_response(200, "Login sucessful", existed_user.user_id) 
+            else:
+                return api_response(400, "Wrong password")
+                
+    except Exception as e:
+        return api_response(500, "Failed to get user", str(e))
         
     
     
